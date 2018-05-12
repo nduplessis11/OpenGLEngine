7c0088a50cb82675328a3a666a5394beca37d0a3
Nicholas DuPlessis <nduplessis11@gmail.com>
2018-05-12 10:50:57 -0400

    Moved grid code to header file to clean up main for now. Might make it a class at some point.

--- OpenGLEngine/src/Application.cpp
+++ OpenGLEngine/src/Application.cpp
@@ -9,128 +9,11 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
-GLfloat grid[308];
+#include "Grid.h"
 
-GLuint gridIndices[] = {
-	0, 1,
-	2, 3,
-	4, 5,
-	6, 7,
-	8, 9,
-	10, 11,
-	12, 13,
-	14, 15,
-	16, 17,
-	18, 19,
-	20, 21,
-
-	22, 23,
-	24, 25,
-	26, 27,
-	28, 29,
-	30, 31,
-	32, 33,
-	34, 35,
-	36, 37,
-	38, 39,
-	40, 41
-};
-
-void fillGrid()
-{
-	GLfloat x = -5.0f;
-	for (int i = 0; i < 154; i+=14)
-	{
-		grid[i] = x;
-		grid[i + 1] = 0.0f;
-		grid[i + 2] = 5.0f;
-
-		if (x == 0.0f)
-		{
-			grid[i + 3] = 1.0f;
-			grid[i + 4] = 0.0f;
-			grid[i + 5] = 0.0f;
-			grid[i + 6] = 1.0f;
-		}
-		else
-		{
-			grid[i + 3] = 0.0f;
-			grid[i + 4] = 1.0f;
-			grid[i + 5] = 0.0f;
-			grid[i + 6] = 1.0f;
-		}
-
-		grid[i + 7] = x;
-		grid[i + 8] = 0.0f;
-		grid[i + 9] = -5.0f;
-
-		if (x == 0.0f)
-		{
-			grid[i + 3] = 1.0f;
-			grid[i + 4] = 0.0f;
-			grid[i + 5] = 0.0f;
-			grid[i + 6] = 1.0f;
-		}
-		else
-		{
-			grid[i + 10] = 0.0f;
-			grid[i + 11] = 1.0f;
-			grid[i + 12] = 0.0f;
-			grid[i + 13] = 1.0f;
-		}
-
-		x += 1.0f;
-	}
-
-	GLfloat z = -5.0f;
-	for (int i = 154; i < 308; i += 14)
-	{
-		grid[i] = 5.0f;
-		grid[i + 1] = 0.0f;
-		grid[i + 2] = z;
-
-		if (z == 0.0f)
-		{
-			grid[i + 10] = 0.0f;
-			grid[i + 11] = 0.0f;
-			grid[i + 12] = 1.0f;
-			grid[i + 13] = 1.0f;
-		}
-		else
-		{
-			grid[i + 3] = 0.0f;
-			grid[i + 4] = 1.0f;
-			grid[i + 5] = 0.0f;
-			grid[i + 6] = 1.0f;
-		}
-
-		grid[i + 7] = -5.0f;
-		grid[i + 8] = 0.0f;
-		grid[i + 9] = z;
-
-		if (z == 0.0f)
-		{
-			grid[i + 10] = 0.0f;
-			grid[i + 11] = 0.0f;
-			grid[i + 12] = 1.0f;
-			grid[i + 13] = 1.0f;
-		}
-		else
-		{
-			grid[i + 10] = 0.0f;
-			grid[i + 11] = 1.0f;
-			grid[i + 12] = 0.0f;
-			grid[i + 13] = 1.0f;
-		}
-
-
-		z += 1.0f;
-	}
-}
 
 int main(void)
 {
-	fillGrid();
 	GLFWwindow* window;
 
 	if (!glfwInit())
@@ -182,11 +65,23 @@ int main(void)
 
 	{
 		Shader shader("res/shaders/shader.vs", "res/shaders/shader.fs");
+
+
+		Grid grid = createGrid(5);
+		GLfloat* gridLines = &grid.gridlines[0].start.x;
+		GLuint* gridIndices = &grid.indices[0].x;
+
+		GLuint floatCount = gridFloatCount(grid);
+		GLuint indexCount = gridIndexCount(grid);
+
+		Mesh meshGrid(gridLines, floatCount, gridIndices, indexCount, GL_LINES);
+		ModelAsset modelAssetGrid{ &shader, meshGrid };
+		ModelInstance modelInstanceGrid{ &modelAssetGrid, glm::mat4(1.0f) };
+
+
 		ModelAsset modelAssetCube { &shader, Mesh(points, sizeof(points) / sizeof(GLfloat), indices, 36, GL_TRIANGLES) };
-		ModelAsset modelAssetGrid { &shader, Mesh(grid, sizeof(grid) / sizeof(GLfloat), gridIndices, 42, GL_LINES) };
 		ModelInstance modelInstanceCube { &modelAssetCube, glm::mat4(1.0f) };
-		ModelInstance modelInstanceGrid { &modelAssetGrid, glm::mat4(1.0f) };
-		
+	
 		std::vector<ModelInstance> modelInstances;
 		modelInstances.push_back(modelInstanceCube);
 		modelInstances.push_back(modelInstanceGrid);
---
+++ OpenGLEngine/src/Grid.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <vector>
+
+#include <glm/glm.hpp>
+
+struct GridLine
+{
+	glm::vec3 start;
+	glm::vec4 start_color;
+	glm::vec3 finish;
+	glm::vec4 finish_color;
+};
+
+struct Grid
+{
+	std::vector<GridLine> gridlines;
+	std::vector<glm::uvec2> indices;
+};
+
+Grid createGrid(int lineCount)
+{
+	int totalLineCount = (lineCount * 2) + 1;
+	Grid grid;
+	float x = -lineCount;
+	float z = lineCount;
+	unsigned int index = 0;
+
+	for (int i = 0; i < totalLineCount; i++)
+	{
+		GridLine gridline{ glm::vec3(x, 0.0f, z), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f), glm::vec3(x, 0.0f, -z), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+		x += 1.0f;
+		grid.gridlines.push_back(gridline);
+		grid.indices.push_back(glm::uvec2(index, index+1));
+		index += 2;
+	}
+
+	x = lineCount;
+	z = -lineCount;
+
+	for (int i = 0; i < totalLineCount; i++)
+	{
+		GridLine gridline{ glm::vec3(x, 0.0f, z), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f), glm::vec3(-x, 0.0f, z), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+		z += 1.0f;
+		grid.gridlines.push_back(gridline);
+		grid.indices.push_back(glm::uvec2(index, index + 1));
+		index += 2;
+	}
+
+	return grid;
+}
+
+unsigned int gridFloatCount(const Grid& grid)
+{
+	unsigned int size = (sizeof(GridLine) * grid.gridlines.size());
+	unsigned int floatCount = size / sizeof(GLfloat);
+
+	return floatCount;
+}
+
+unsigned int gridIndexCount(const Grid& grid)
+{
+	unsigned int size = (sizeof(glm::uvec2) * grid.indices.size());
+	unsigned int indexCount = size / sizeof(GLuint);
+
+	return indexCount;
+}
